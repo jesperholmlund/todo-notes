3f0d6a5c4864ecc7144736173c2e301fe8f4bcc2
jesperholmlund <jesperholmlund@student.kyh.se>
2021-12-03 11:43:52 +0100

    comments js

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,24 @@
 window.onload = function () {
+  //hämtar innehållet i input för todo samt radioknapparna
   let todoTitle = document.getElementById("todoTitle");
   let prioRadios = document.getElementsByClassName("prioRadio");
 
+  //lägger en klicklyssnare på knappen man lägger till en ny todo
   document.getElementById("addBtn").addEventListener("click", function () {
-    numb = 0;
+    //en loop för radio knapparna. hämtar ut alla tre
     for (let i = 0; i < prioRadios.length; i++) {
+      //if sats för att kolla om innehållet på input är tre eller högre
+      //lägre skrivs ett felmeddelande ut
       if (todoTitle.value.length < 3) {
         document.getElementById("failMessage").style.display = "block";
         document.getElementById("failMessage").innerHTML =
           "Too short. Most be at least 3 characters";
       } else {
+        //om tre eller högre göms failmessage
         document.getElementById("failMessage").style.display = "none";
+        //sen tre if satser som kollar vilken checkbox som är klickad
+        //skriver då ut olika meddelande, low medium eller high
+        //samt lägger till en klass med olika färger beroende på prioritet
         if (prioRadios[i].checked == true) {
           if (i === 0) {
             todoListBody.innerHTML += `<div class="todoItem"><div class="todoTitle"> ${todoTitle.value}</div>
@@ -34,14 +42,17 @@ window.onload = function () {
       `;
           }
         }
+        //kallar en funktion som raderar
         deleteTodo();
       }
     }
   });
   function deleteTodo() {
+    //hämtar rätt deleteknapp
     let deleteBtn = document.getElementsByClassName("delBtn");
     for (let i = 0; i < deleteBtn.length; i++) {
       deleteBtn[i].addEventListener("click", function () {
+        //this pekar på delteknappen, sen hoppar den till två parent element och tar bort det
         this.parentNode.parentNode.remove();
       });
     }
--- javascript/notes.js
+++ javascript/notes.js
@@ -5,22 +5,30 @@ window.onload = function () {
       this.content = content;
     }
   }
+  //hämtar alla relevanta variabler
   let addNote = document.getElementById("addNoteBtn");
   let noteTitleAdd = document.getElementById("noteTitleAdd");
   let noteContentAdd = document.getElementById("noteContentAdd");
   let noteTitle = document.getElementById("noteTitle");
   let noteContent = document.getElementById("noteContent");
+  //skapar en array för noterna och pushar in klassobjekt dit
   const notes = [];
+
   addNote.addEventListener("click", function () {
+    //if sats som kollar om det saknar titel eller content
+    //fattas det kommer failmessage att synas
     if (noteTitleAdd.value == "" || noteContentAdd == "") {
       document.getElementById("failMessage").style.display = "block";
       document.getElementById("failMessage").innerHTML =
         "You need both titlel and content!";
     } else {
+      //annars döljs failmessage
+      //och pushar in klassobjektet i arrayen
       document.getElementById("failMessage").style.display = "none";
       notes.push(new Note(noteTitleAdd.value, noteContentAdd.value));
-      console.log(notes);
+      //skriver ut titeln för noten på relevant ställe
       noteTitle.innerHTML += `<div class="noteListTitle"><i class="fa fa-trash"></i> ${noteTitleAdd.value}</div>`;
+      //kalalr funktion som skriver ut content
       noteList();
     }
   });
@@ -28,7 +36,9 @@ window.onload = function () {
   let noteListTitle = document.getElementsByClassName("noteListTitle");
 
   function noteList() {
+    //loop som kolar genom alla titlar och varje titel får ett indexnummer
     for (let i = 0; i < noteListTitle.length; i++) {
+      //använder samma indexnummer för att hämta från rätt klassobjekt (det med samma indexposition)
       noteListTitle[i].addEventListener("click", function () {
         noteContent.innerHTML = `<h3>${notes[i].title}</h3><p>${notes[i].content}</p>`;
       });
